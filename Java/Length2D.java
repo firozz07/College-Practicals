f845d370283d4e1333903cf86d48e5066fe0239a
firoz <firozz0007@gmail.com>
2026-07-18 00:25:36 +0530

    2d array printed for the 2nd time

---
+++ Java/Length2D.java
@@ -0,0 +1,19 @@
+package Java;
+
+public class Length2D {
+    public static void main(String[] args) {
+        int [][]arr={
+            {1,2,3},
+            {4,5,6},
+            {7,8,9}
+        };
+        int rowSize=arr.length;
+        int colSize=arr[0].length;
+        for (int i = 0; i < rowSize; i++) {
+            for (int j = 0; j < colSize; j++) {
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+}
